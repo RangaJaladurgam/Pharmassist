3b4ade7f49e5d3bc5ca4d7484c3bd34c9bd2ab06
RangaJaladurgam <rangajaladurgam7258@gmail.com>
2024-10-28 18:33:41 +0530

    creating find Admin by id in Admin Controller

--- src/main/java/com/pharmassist/controller/AdminController.java
+++ src/main/java/com/pharmassist/controller/AdminController.java
@@ -1,20 +1,29 @@
 package com.pharmassist.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
+import com.pharmassist.entity.Admin;
 import com.pharmassist.service.AdminService;
+import com.pharmassist.util.AppResponseBuilder;
+import com.pharmassist.util.ResponseStructure;
 
 @Controller
 public class AdminController {
 	
 	private final AdminService adminService;
+	private final AppResponseBuilder response;
 
-	public AdminController(AdminService adminService) {
+	public AdminController(AdminService adminService,AppResponseBuilder response) {
 		this.adminService = adminService;
+		this.response = response;
 	}
 	
 	
-	
+	public ResponseEntity<ResponseStructure<Admin>> findAdmin(int adminId){
+		Admin admin = adminService.findUser(adminId);
+		return response.success(HttpStatus.FOUND,"Admin found by Id", admin);
+	}
 	
 }
