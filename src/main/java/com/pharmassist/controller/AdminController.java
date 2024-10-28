b53b8eed52b79203008aa6aea1f897a95e51b9dd
RangaJaladurgam <rangajaladurgam7258@gmail.com>
2024-10-28 22:31:26 +0530

    Fix : Updated saveAdmin in controller

--- src/main/java/com/pharmassist/controller/AdminController.java
+++ src/main/java/com/pharmassist/controller/AdminController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.pharmassist.entity.Admin;
 import com.pharmassist.requestdto.AdminRequest;
@@ -26,7 +27,7 @@ public class AdminController {
 	}
 	
 	@PostMapping("/admins")
-	public ResponseEntity<ResponseStructure<AdminResponse>> saveAdmin(AdminRequest adminRequest){
+	public ResponseEntity<ResponseStructure<AdminResponse>> saveAdmin(@RequestBody AdminRequest adminRequest){
 		AdminResponse adminResponse =  adminService.saveAdmin(adminRequest);
 		return response.success(HttpStatus.CREATED, "Admin Created", adminResponse);
 	}
