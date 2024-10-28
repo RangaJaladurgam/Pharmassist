50b490321242d42c7e79444e134c66fbb2137cdd
RangaJaladurgam <rangajaladurgam7258@gmail.com>
2024-10-28 21:22:30 +0530

    Creating Admin Request and Response DTOs

--- src/main/java/com/pharmassist/service/AdminService.java
+++ src/main/java/com/pharmassist/service/AdminService.java
@@ -2,7 +2,21 @@ package com.pharmassist.service;
 
 import org.springframework.stereotype.Service;
 
+import com.pharmassist.entity.Admin;
+import com.pharmassist.repository.AdminRepository;
+
 @Service
 public class AdminService {
+
+	private AdminRepository adminRepository;
+
+	public AdminService(AdminRepository adminRepository) {
+		super();
+		this.adminRepository = adminRepository;
+	}
+
+
+	
+	
 
 }
